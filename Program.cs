f3ebc1ff649d2716130d80f5c7902a2035b77466
avandehoef <avandehoef@outlook.com>
2017-05-31 23:05:46 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double testNummer;
+            double nummer = 2;
+           
+            for (testNummer = 600851475143; nummer < testNummer; testNummer = testNummer / nummer)
+            {
+                if (testNummer % nummer == 0)
+                {
+                    continue;                    
+                }
+                else
+                {
+                   for (testNummer = testNummer; nummer <= testNummer; nummer = nummer + 1)
+                    {
+                        if (testNummer % nummer != 0 || nummer == 1)
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            Console.WriteLine(nummer);
+                            break;                            
+                        }
+                    }
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
